0f40a8f18fda28e7029a2f22b16a8ace0e5fb8ac
Abulashvili27 <davit.abulashvili.1@btu.edu.ge>
2021-02-02 21:08:29 +0400

    Registration page created

--- myapplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ myapplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,9 +2,20 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void onWelcomeClick(View view){
+        Intent changeActivity = new Intent(this, registrationActivity.class);
+        startActivity(changeActivity);
+        overridePendingTransition(R.anim.slide_down, R.anim.slide_up);
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
